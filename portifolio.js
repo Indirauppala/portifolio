980ddceadeccd5b4b4a31cd8c2ee8a3b58b0e159
Indirauppala <140385777+Indirauppala@users.noreply.github.com>
2024-04-06 16:05:35 +0530

    Add files via upload

---
+++ portifolio.js
@@ -0,0 +1,6 @@
+document.getElementById("readmore").addEventListener("click", function() {
+    document.getElementById("matter").classList.remove("hidden"); // Remove the 'hidden' class
+});
+function closePage(){
+    window.close();
+}
